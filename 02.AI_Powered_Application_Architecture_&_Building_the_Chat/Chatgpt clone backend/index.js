dd4c4150de09a20c258bf8e6dacf05e35a9eda51
Sebah Abrar <sebahabr69@gmail.com>
2026-09-04 19:26:58 -0700

    refactor: reorganize project folder structure

--- 02.AI_Powered_Application_Architecture_&_Building_the_Chat/Chatgpt clone backend/index.js
+++ 02.AI_Powered_Application_Architecture_&_Building_the_Chat/Chatgpt clone backend/index.js
@@ -7,6 +7,8 @@ const app = express();
 
 async function startServer(){
     try{
+      const connection = await db.getConnection();
+      connection.release();
   app.listen(8000,(err)=>{
     if(err){
       throw err;
