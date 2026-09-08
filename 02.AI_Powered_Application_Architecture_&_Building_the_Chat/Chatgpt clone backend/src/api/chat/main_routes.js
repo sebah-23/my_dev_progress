87a22a706f03e84a2c064b41f8aba940c11350b1
Sebah Abrar <sebahabr69@gmail.com>
2026-09-07 17:16:34 -0700

    refactor: reorganize project folder structure

--- 02.AI_Powered_Application_Architecture_&_Building_the_Chat/Chatgpt clone backend/src/api/chat/main_routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'; 
-import chatRouter from './chat_routes.js';
-const mainRouter  = express.Router();
-
-
-mainRouter.use('/chat', chatRouter);
-
-export default mainRouter
